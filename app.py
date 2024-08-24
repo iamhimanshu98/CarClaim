5d7376aec37c5cfe3d967c77f2e56e8121907d16
Himanshu Kumawat <135202238+iamhimanshu98@users.noreply.github.com>
2024-08-24 13:23:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import joblib
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
 
-# model = joblib.load('./models/randomForest.lb')
+filename = 'label_encoder.pkl'
+loaded_encoder = joblib.load(filename)
+model = joblib.load('./model/decision_tree_model.lb')
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,58 +17,72 @@ def home():
 def form():
     return render_template('form.html')
 
-# @app.route("/prediction",methods=['GET','POST'])
-# def prediction():
-#     if request.method == "POST":
-#         brand = request.form['brand']
-#         category = request.form['category']
-#         processor_brand = request.form['processor_brand']
-#         ram = request.form['ram']
-#         processor_name = request.form['processor_name']
-#         operating_system = request.form['operating_system']
-#         generation = request.form['processor_generation']
-#         architecture = request.form['architecture']
-#         ssd = request.form['ssd']
-#         touch_screen = request.form['touch_screen']
+@app.route("/prediction",methods=['GET','POST'])
+def prediction():
+    if request.method == "POST":
+        age = request.form['age'] 
+        sex = request.form['sex'] 
+        education_level = request.form['education_level']   #
+        premium = request.form['premium']
+        deductable = request.form['deductable']
+        incident_type = request.form['incident_type']   #
+        collision_type = request.form['collision_type']     #
+        incident_severity = request.form['incident_severity']   #
+        authorities_contacted = request.form['authorities_contacted']   #
+        property_damage = request.form['property_damage']       
+        vehicles_involved = request.form['vehicles_involved']
+        bodily_injuries = request.form['bodily_injuries']
+        witnesses = request.form['witnesses']
+        injury_claim = request.form['injury_claim']
+        vehicle_claim = request.form['vehicle_claim']
         
-#         category_list = ['ThinNline' , 'Gaming']
-#         processor_brand_list = ['Intel', 'M1']
-#         operating_system_list = [ 'Mac', 'Windows']
-#         processor_name_list = ['Core i3', 'Core i5', 'Core i7', 'Core i9', 'Ryzen r3', 'Ryzen 5', 'Ryzen 7', 'Ryzen 9', 'M1', 'Pentium Quad']
-#         ram_list = ['4 Gb','8 Gb','32 Gb']
-#         brand_list = ['Acer', 'Asus', 'Dell', 'HP', 'Lenovo', 'MSI', 'Avita']
-#         generation_list = ['12th', '11th', '9th', '8th', '7th' , '4th', 'Not Available']
-#         ssd_list = ['128 GB','256 GB','512 GB','1024 GB','2048 GB','3072 GB']
+        # education_level_list = ['JD', 'High School', 'Associate', 'MD', 'Masters', 'PhD', 'College']
+        # encoded_educational_level_list = encoder.transform(education_level_list)
         
-#         brand_flag = [1 if brand == b else 0 for b in brand_list]
-#         category_flag = [1 if category == c else 0 for c in category_list]
-#         processor_brand_flag = [1 if processor_brand == pb else 0 for pb in processor_brand_list]
-#         ram_flag = [1 if ram == r else 0 for r in ram_list]
-#         processor_name_flag = [1 if processor_name == pn else 0 for pn in processor_name_list]
-#         operating_system_flag = [1 if operating_system == os else 0 for os in operating_system_list]
-#         generation_flag = [1 if generation == gn else 0 for gn in generation_list]
-#         ssd_flag = [1 if ssd == s else 0 for s in ssd_list]
-#         touchscreen_flag = 1 if touch_screen == 'Yes' else 0
-#         architecture_flag = 1 if architecture == '64 Bit' else 0
-
-#         unseen_data = [architecture_flag,touchscreen_flag] + brand_flag + category_flag + processor_brand_flag + ram_flag + processor_name_flag + operating_system_flag + generation_flag + ssd_flag
+        encoded_educational_level = 0
+        encoded_incident_type = 0
+        encoded_collision_type = 0
+        encoded_incident_severity = 0
+        encoded_authorities_contacted = 0
         
-#         prediction = model.predict([unseen_data])
-#         output = round(prediction[0],2)
+        unseen_data = [[age,
+                        sex,
+                        encoded_educational_level,
+                        premium,
+                        deductable,
+                        encoded_incident_type,
+                        encoded_collision_type,
+                        encoded_incident_severity,
+                        encoded_authorities_contacted,
+                        property_damage,
+                        vehicles_involved,
+                        bodily_injuries,
+                        witnesses,
+                        injury_claim,
+                        vehicle_claim
+                        ]]
+        
+        prediction = model.predict(unseen_data)
+        output = prediction[0]
     
-#         return render_template('result.html',
-#             output=output,
-#             architecture = architecture,
-#             touch_screen = touch_screen,
-#             brand = brand,
-#             category = category,
-#             processor_brand = processor_brand,
-#             ram = ram,
-#             processor_name = processor_name,
-#             operating_system = operating_system,
-#             generation = generation,
-#             ssd = ssd,
-#             )
+        return render_template('result.html',
+            output = 'Fraud' if output == 'Y' else 'Not Fraud',
+            age = age,
+            sex = 'Male' if 1 == sex else 'Female',
+            education_level = education_level,
+            premium = premium,
+            deductable = deductable,
+            incident_type = incident_type,
+            collision_type = collision_type,
+            incident_severity = incident_severity,
+            authorities_contacted = authorities_contacted,
+            property_damage = 'Yes' if property_damage == 1 else 'No',
+            vehicles_involved = vehicles_involved,
+            bodily_injuries = bodily_injuries,
+            witnesses = witnesses,
+            injury_claim = injury_claim,
+            vehicle_claim = vehicle_claim
+            )
 
 if __name__ == "__main__":
     app.run(debug=True)
