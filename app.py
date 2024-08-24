f8fa9ad3edad7d569e3484540bfe599d6e9d002a
Himanshu Kumawat <135202238+iamhimanshu98@users.noreply.github.com>
2024-08-24 14:25:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, request
 import joblib
-from sklearn.preprocessing import LabelEncoder
-encoder = LabelEncoder()
-
-filename = 'label_encoder.pkl'
-loaded_encoder = joblib.load(filename)
 model = joblib.load('./model/decision_tree_model.lb')
 
 app = Flask(__name__)
@@ -20,30 +15,42 @@ def form():
 @app.route("/prediction",methods=['GET','POST'])
 def prediction():
     if request.method == "POST":
-        age = request.form['age'] 
-        sex = request.form['sex'] 
+        age = int(request.form['age']) 
+        sex = int(request.form['sex']) 
         education_level = request.form['education_level']   #
-        premium = request.form['premium']
-        deductable = request.form['deductable']
+        premium = int(request.form['premium'])
+        deductable = int(request.form['deductable'])
         incident_type = request.form['incident_type']   #
         collision_type = request.form['collision_type']     #
         incident_severity = request.form['incident_severity']   #
         authorities_contacted = request.form['authorities_contacted']   #
-        property_damage = request.form['property_damage']       
-        vehicles_involved = request.form['vehicles_involved']
-        bodily_injuries = request.form['bodily_injuries']
-        witnesses = request.form['witnesses']
-        injury_claim = request.form['injury_claim']
-        vehicle_claim = request.form['vehicle_claim']
+        property_damage = int(request.form['property_damage'])       
+        vehicles_involved = int(request.form['vehicles_involved'])
+        bodily_injuries = int(request.form['bodily_injuries'])
+        witnesses = int(request.form['witnesses'])
+        injury_claim = int(request.form['injury_claim'])
+        vehicle_claim = int(request.form['vehicle_claim'])
         
-        # education_level_list = ['JD', 'High School', 'Associate', 'MD', 'Masters', 'PhD', 'College']
-        # encoded_educational_level_list = encoder.transform(education_level_list)
+        education_level_dict = {'JD':3, 'High School':2, 'Associate':0, 'MD':4, 'Masters':5, 'PhD':6, 'College':1}
+        incident_type_dict = {'Multi-vehicle Collision':0, 'Single Vehicle Collision':2, 'Vehicle Theft':3, 'Parked Car':1} 
+        collision_type_dict = {'Rear Collision':1, 'Side Collision':2, 'Front Collision':0}
+        incident_severity_dict = {'Minor Damage':1, 'Total Loss':2, 'Major Damage':0, 'Trivial Damage':3}
+        authorities_contacted_dict = {'Police':3, 'Fire':1, 'Other':2, 'Ambulance':0}
         
-        encoded_educational_level = 0
-        encoded_incident_type = 0
-        encoded_collision_type = 0
-        encoded_incident_severity = 0
-        encoded_authorities_contacted = 0
+        if education_level in education_level_dict:
+            encoded_educational_level = education_level_dict[education_level]
+
+        if incident_type in incident_type_dict:
+            encoded_incident_type = incident_type_dict[incident_type]
+
+        if collision_type in collision_type_dict:
+            encoded_collision_type = collision_type_dict[collision_type]
+
+        if incident_severity in incident_severity_dict:
+            encoded_incident_severity = incident_severity_dict[incident_severity]
+        
+        if authorities_contacted in authorities_contacted_dict:
+            encoded_authorities_contacted = authorities_contacted_dict[authorities_contacted]
         
         unseen_data = [[age,
                         sex,
@@ -68,7 +75,7 @@ def prediction():
         return render_template('result.html',
             output = 'Fraud' if output == 'Y' else 'Not Fraud',
             age = age,
-            sex = 'Male' if 1 == sex else 'Female',
+            sex = 'Male' if sex == 1 else 'Female',
             education_level = education_level,
             premium = premium,
             deductable = deductable,
@@ -76,7 +83,7 @@ def prediction():
             collision_type = collision_type,
             incident_severity = incident_severity,
             authorities_contacted = authorities_contacted,
-            property_damage = 'Yes' if property_damage == 1 else 'No',
+            property_damage = 'Yes' if property_damage == '1' else 'No',
             vehicles_involved = vehicles_involved,
             bodily_injuries = bodily_injuries,
             witnesses = witnesses,
